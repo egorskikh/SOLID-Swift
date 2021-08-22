20576a7e7cd19d71ca99cd391d4128d866121017
Egor Gorskikh <egorgorskikh@gmail.com>
2021-08-22 16:27:30 +0300

    feat: OCP

--- OCP.playground/Contents.swift
+++ OCP.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Developer - абстракция
+protocol Developer {
+    // Открытый интерфейс с доступными методами
+    // Каждому Developer мы можем дать задачу:
+    func make(program: String) -> String
+}
+
+class SwiftDeveloper: Developer {
+    // SwiftDeveloper подписан на протокол Developer, поэтому
+    // его открытый интерфейс это метод makeProgramm
+    func make(program: String) -> String {
+        return ""
+    }
+
+    // И никто не знает, что ищут в Google
+    private func searchInGoogle(task: String) -> String {
+        return ""
+    }
+}
