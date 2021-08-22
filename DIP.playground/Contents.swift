d3bfc6215e6ec7f3d989a3d813de05e6abb89c0f
Egor Gorskikh <egorgorskikh@gmail.com>
2021-08-22 16:52:55 +0300

    feat: DIP

--- DIP.playground/Contents.swift
+++ DIP.playground/Contents.swift
@@ -1,3 +1,58 @@
 import UIKit
+import CoreData
 
-var greeting = "Hello, playground"
+class NotUser {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class CoreData {
+    func save(user: User) {
+        // Save user on database
+    }
+}
+
+class NotUserService {
+    func save(user: User) {
+        let database = CoreData()
+        database.save(user: user)
+    }
+}
+
+/*
+ Создание экземпляра класса, как мы это сделали в примере, генерирует сильную связь, поэтому,
+ если мы хотим использовать другую базу данных, нам придется переделать код.
+
+ Чтобы решить эту проблему, мы можем сделать то, что объяснялось в предыдущей статье,
+ когда был установлен уровень базы данных.
+ */
+
+// Решаем проблему
+
+protocol Storable { }
+
+extension NSObject: Storable { } // Realm Database
+extension NSManagedObject: Storable { } // Core Data Database
+
+protocol StorageManager {
+  /// Save Object into Realm database
+  /// - Parameter object: Realm object (as Storable)
+  func save(object: Storable)
+}
+
+class User: Storable {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class UserService: StorageManager {
+    func save(object: Storable) {
+        // Saves user to database
+    }
+}
