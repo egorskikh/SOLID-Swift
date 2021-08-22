1c231a021992e22462ed70cce83dc3a608ff3323
Egor Gorskikh <egorgorskikh@gmail.com>
2021-08-22 16:19:23 +0300

    feat: SRP

--- SRP.playground/Contents.swift
+++ SRP.playground/Contents.swift
@@ -1,3 +1,37 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// MARK: - 👎🏻 SOLID - SRP
+
+class BadDeveloper {
+
+    var programms: [AnyHashable]?
+
+    func write(_ programm: String) {
+        // ...
+    }
+
+    func test(_ programm: String) {
+        // ...
+    }
+
+}
+
+// MARK: - 👍🏻 SOLID - SRP
+
+class QA {
+
+    func test(programm: String) {
+        // ...
+    }
+
+}
+
+class Developer {
+
+    var programms: [AnyHashable]?
+
+    func write(programm: String) {
+        // ...
+    }
+
+}
