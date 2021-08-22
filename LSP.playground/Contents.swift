7f8adbff8e6091816007f426a5abe0956412c50d
Egor Gorskikh <egorgorskikh@gmail.com>
2021-08-22 16:33:16 +0300

    feat: LSP

--- LSP.playground/Contents.swift
+++ LSP.playground/Contents.swift
@@ -1,3 +1,41 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// 1. Определим протокол для всего, чем можно рисовать.
+protocol Drawing {
+    var color: UIColor { get }
+}
+
+// 2. Определим подклассы.
+// Класс Ручки.
+class Pen: Drawing {
+    var color: UIColor
+
+    init(color: UIColor) {
+        self.color = color
+    }
+}
+
+// Класс Карандаша.
+class Pencil: Drawing {
+    var color: UIColor
+
+    init(color: UIColor) {
+        self.color = color
+    }
+}
+
+// 3. Класс Ребенка, который может рисовать всем, что может рисовать.
+class Child {
+    func drawHouse(with item: Drawing) {
+        print("House color: ", item.color)
+    }
+}
+
+// Экземпляры классов: Карандаша, Ручки и Ребенка.
+let bluePen = Pen(color: .blue)
+let blackPencil = Pencil(color: .black)
+let bob = Child()
+
+// Проверим, что Ребенок Боб может рисовать и ручками и карандашами
+bob.drawHouse(with: bluePen)        // House color: blue
+bob.drawHouse(with: blackPencil)    // House color: black
